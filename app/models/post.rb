0c4ef968b6654d16e7b9d11aaae94e13defd0ca6
Jakub Adamczyk <andrzej@golonka.pl>
2014-10-26 10:47:45 +0100

    Fix one

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ActiveRecord::Base
   
-  after_save :set_visible_to_false
+  after_save :set_visible_to_true
   validates :body, presence: true
   
 protected
-  def set_visible_to_false
-    self.visible = false
-    self.save
+  def set_visible_to_true
+    self.visible = true
   end
   
 end
